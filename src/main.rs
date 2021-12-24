6dc30599cb912c9db29ed0a6c79d6a8286bc9051
willian juliate <willianjuliate@gmail.com>
2021-12-24 13:14:26 -0300

    Reformulado o funcao de input, agr pode receber um prefixo antes do input do usuario

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod new_folder;
 use new_folder::NewFolder;
-use std::io;
+use std::io::{stdin, stdout, Write};
 use std::process::Command;
 
 fn main() {
@@ -8,11 +8,11 @@ fn main() {
     let mut new = NewFolder::new();
     println!("====================== BEM-VINDO AO DIR BUILDER =======================");
     println!("INFORME O NOME DO PROJETO");
-    new.set_name_project(input());
+    new.set_name_project(input("> "));
 
     println!("\n--------------------------------- MÓDULOS ---------------------------------");
     println!("INFORME A QUANTIDADE DE MÓDULOS");
-    match input().parse() {
+    match input("> ").parse() {
         Ok(n) => new.set_quantity_modules(n),
         Err(_) => println!("VALOR PADRÃO DEFINIDO '0' "),
     };
@@ -22,12 +22,12 @@ fn main() {
             "ADICIONAR NOVA(S) PASTA(S) (A), DELETAR PASTA(S) PADRÃO (D) OU DEIXE VAZIO PARA SAIR"
         );
 
-        match &*input() {
+        match &*input("> ") {
             "A" | "C" | "ADD" | "CRIAR" | "ADICIONAR" => {
                 //println!("LISTA DAS PASTAS PADRÕES");
                 new.show_dirs();
                 println!("ADD: INFORME O NOME DA(S) NOVA(S) PASTAS SEPARADAS POR VIRGULAS (,)");
-                new.add_new_folders(input());
+                new.add_new_folders(input("> "));
                 new.show_dirs();
                 continue;
             }
@@ -37,7 +37,7 @@ fn main() {
                 println!(
                     "DEL: INFORME O NOME DA(S) PASTAS A SER DELETADAS SEPARADAS POR VIRGULA (,)"
                 );
-                new.remove_folders(input());
+                new.remove_folders(input("> "));
                 new.show_dirs();
                 continue;
             }
@@ -47,17 +47,19 @@ fn main() {
                 break;
             }
             _ => {
-                println!("OPÇÃO INVÁLIDA");
+                println!("# OPÇÃO INVÁLIDA #");
                 continue;
             }
         };
     }
     let _ = Command::new("cmd.exe").arg("/c").arg("pause").status();
 }
-fn input() -> String {
+fn input(prfix: &str) -> String {
     let mut str = String::new();
-    io::stdin()
+    print!("{}", prfix);
+    let _ = stdout().flush();
+    stdin()
         .read_line(&mut str)
-        .expect("ERRO AO LER A ENTRADA!");
+        .expect("Não inseriu uma string correta!");
     str.trim().to_uppercase()
 }
